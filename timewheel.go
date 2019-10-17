7840b2592e0cd74c7ea16c635e989b3e5580912b
lilin <lilin-2@thunisoft.com>
2019-10-17 16:21:18 +0800

    由chan处理task,改为lock

--- timewheel.go
+++ timewheel.go
@@ -4,21 +4,11 @@ import (
 	"container/list"
 	"errors"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
 type (
-	bucket = *list.List
-	taskAc = int
 	tasKFn = func()
-)
-
-const (
-	addTaskAction = iota + 1
-	delTaskAction
-	reLoadTask
-	updateTaskAction
 )
 
 const (
@@ -28,34 +18,30 @@ const (
 )
 
 const (
-	maxBuckets            uint32 = 1024 * 1024
-	defaultTaskFnChanSize        = 16
+	maxBuckets uint32 = 1024 * 1024
 )
 
 var (
 	IllegalBucketNumError = errors.New("illegal bucket num")
 	IllegalTaskDelayError = errors.New("illegal delay time ")
 	TaskKeyExistError     = errors.New("task key already exists")
-	NotRunError           = errors.New("timeWheel not run")
+	NotRunError           = errors.New("timeWheel not running")
 )
 
 type TimeWheel struct {
-	ticker             *time.Ticker
-	tickDuration       time.Duration
-	bucketsNum         uint32
-	buckets            []bucket
-	curPos             uint32
-	keyPosMap          map[string]uint32
-	taskChannel        chan taskAction
-	taskExecuteChannel chan tasKFn
-	status             int32
-	begin              sync.Once
-	end                sync.Once
+	ticker       *time.Ticker
+	tickDuration time.Duration
+	bucketsNum   uint32
+	buckets      []bucket
+	curPos       uint32
+	keyPosMap    sync.Map
+	status       int32
+	begin        sync.Once
 }
 
-type taskAction struct {
-	task   *task
-	action taskAc
+type bucket struct {
+	list *list.List
+	mu   sync.Mutex
 }
 
 type task struct {
@@ -65,7 +51,18 @@ type task struct {
 	circle   uint32
 	fn       tasKFn
 	schedule bool
-	e        *list.Element
+}
+
+func (b *bucket) remove(e *list.Element) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.list.Remove(e)
+}
+
+func (b *bucket) push(t *task) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.list.PushBack(t)
 }
 
 func New(tickDuration time.Duration, bucketsNum uint32) (*TimeWheel, error) {
@@ -76,23 +73,19 @@ func New(tickDuration time.Duration, bucketsNum uint32) (*TimeWheel, error) {
 	num := normalizeTicksPerWheel(bucketsNum)
 
 	tw := &TimeWheel{
-		tickDuration:       tickDuration,
-		bucketsNum:         num,
-		buckets:            make([]bucket, num),
-		keyPosMap:          make(map[string]uint32),
-		curPos:             0,
-		taskChannel:        make(chan taskAction),
-		taskExecuteChannel: make(chan tasKFn, defaultTaskFnChanSize),
-		status:             ready,
+		tickDuration: tickDuration,
+		bucketsNum:   num,
+		buckets:      make([]bucket, num),
+		curPos:       0,
+		status:       ready,
 	}
 
 	for i := 0; i < int(num); i++ {
-		tw.buckets[i] = list.New()
+		tw.buckets[i] = bucket{list: list.New()}
 	}
 	return tw, nil
 }
 
-// add once task
 func (tw *TimeWheel) AddTask(key string, delay time.Duration, fn func()) error {
 	err := tw.addTask(key, delay, false, fn)
 	if err != nil {
@@ -101,13 +94,6 @@ func (tw *TimeWheel) AddTask(key string, delay time.Duration, fn func()) error {
 	return nil
 }
 
-// remove the task by key
-func (tw *TimeWheel) RemoveTask(taskKey string) {
-	task := &task{key: taskKey}
-	tw.taskChannel <- taskAction{task, delTaskAction}
-}
-
-// add schedule task
 func (tw *TimeWheel) AddScheduleTask(key string, delay time.Duration, fn func()) error {
 	err := tw.addTask(key, delay, true, fn)
 	if err != nil {
@@ -116,32 +102,54 @@ func (tw *TimeWheel) AddScheduleTask(key string, delay time.Duration, fn func())
 	return nil
 }
 
-// add task
 func (tw *TimeWheel) addTask(key string, delay time.Duration, schedule bool, fn func()) error {
-	if _, exist := tw.keyPosMap[key]; exist {
+	if tw.status != run {
+		return NotRunError
+	}
+
+	if _, exist := tw.keyPosMap.Load(key); exist {
 		return TaskKeyExistError
 	}
 	if delay <= 0 {
 		return IllegalTaskDelayError
 	}
-	if atomic.LoadInt32(&tw.status) != run {
-		return NotRunError
-	}
+
 	if delay < tw.tickDuration {
 		delay = tw.tickDuration
 	}
-
+	pos, circle := tw.getPositionAndCircle(delay)
 	task := &task{
 		delay:    delay,
 		key:      key,
+		pos:      pos,
+		circle:   circle,
 		fn:       fn,
 		schedule: schedule,
 	}
-	tw.taskChannel <- taskAction{task, addTaskAction}
+	tw.buckets[pos].push(task)
+	tw.keyPosMap.Store(key, pos)
 	return nil
 }
 
-// get task pos and circle
+func (tw *TimeWheel) RemoveTask(key string) {
+
+	pos, ok := tw.keyPosMap.Load(key)
+	if !ok {
+		return
+	}
+	bucket := tw.buckets[pos.(uint32)]
+	var N *list.Element
+	for e := bucket.list.Front(); e != nil; e = N {
+		N = e.Next()
+		task := e.Value.(*task)
+		if key == task.key {
+			bucket.remove(e)
+			tw.keyPosMap.Delete(key)
+			return
+		}
+	}
+}
+
 func (tw *TimeWheel) getPositionAndCircle(delay time.Duration) (pos uint32, circle uint32) {
 	dd := uint32(delay / tw.tickDuration)
 	circle = dd / tw.bucketsNum
@@ -158,19 +166,13 @@ func (tw *TimeWheel) Start() {
 		tw.ticker = time.NewTicker(tw.tickDuration)
 		tw.status = run
 		go tw.startTickerHandle()
-		go tw.startTaskActionHandle()
-		go tw.starTaskFnExecuteHandle()
 	})
 }
 
-// stop the timeWheel
 func (tw *TimeWheel) Stop() {
-	tw.end.Do(func() {
-		tw.status = stop
-	})
+	tw.status = stop
 }
 
-// start ticker handler
 func (tw *TimeWheel) startTickerHandle() {
 	for {
 		select {
@@ -185,95 +187,31 @@ func (tw *TimeWheel) startTickerHandle() {
 	}
 }
 
-// start task action handler
-func (tw *TimeWheel) startTaskActionHandle() {
-	for {
-		select {
-		case taskAction := <-tw.taskChannel:
-			tw.handleTask(taskAction)
-		default:
-			if tw.status == stop {
-				return
-			}
-		}
-	}
-}
-
-// start task func execute handler
-func (tw *TimeWheel) starTaskFnExecuteHandle() {
-	for {
-		select {
-		case tasKFn := <-tw.taskExecuteChannel:
-			tasKFn()
-		default:
-			if tw.status == stop {
-				return
-			}
-		}
-	}
-}
-
-// the ticker handler
 func (tw *TimeWheel) handleTicker() {
-	tw.curPos = (tw.curPos + 1) & (tw.bucketsNum - 1)
-	l := tw.buckets[tw.curPos]
+	tw.curPos = (tw.curPos + 1) & (tw.bucketsNum - 1) //equals (tw.curPos + 1) % tw.bucketsNum
+	bucket := tw.buckets[tw.curPos]
 
-	for e := l.Front(); e != nil; e = e.Next() {
+	var N *list.Element
+	for e := bucket.list.Front(); e != nil; e = N {
+		N = e.Next()
 		task := e.Value.(*task)
-		task.e = e
-
 		if task.circle > 0 {
 			task.circle--
 			continue
 		}
+		task.fn()
+		bucket.remove(e)
 
-		tw.taskExecuteChannel <- task.fn
-		tw.taskChannel <- taskAction{task, reLoadTask}
-	}
-}
-
-// the task handler
-func (tw *TimeWheel) handleTask(action taskAction) {
-	tk := action.task
-	ac := action.action
-	switch ac {
-	case addTaskAction:
-		tw.addTaskHandle(tk)
-	case reLoadTask:
-		l := tw.buckets[tk.pos]
-		l.Remove(tk.e)
-		if !tk.schedule {
-			delete(tw.keyPosMap, tk.key)
-		}
-		tw.addTaskHandle(tk)
-	case delTaskAction:
-		tw.removeTaskHandle(tk.key)
-	}
-}
-
-// execute add the task
-func (tw *TimeWheel) addTaskHandle(task *task) {
-	pos, circle := tw.getPositionAndCircle(task.delay)
-	task.pos = pos
-	task.circle = circle
-	task.e = nil
-	tw.buckets[pos].PushBack(task)
-	tw.keyPosMap[task.key] = pos
-}
-
-// execute remove the task by key
-func (tw *TimeWheel) removeTaskHandle(key string) {
-	pos, ok := tw.keyPosMap[key]
-	if !ok {
-		return
-	}
-	l := tw.buckets[pos]
-	for e := l.Front(); e != nil; e = e.Next() {
-		task := e.Value.(*task)
-		if key == task.key {
-			l.Remove(e)
-			delete(tw.keyPosMap, key)
-			return
+		if task.schedule {
+			// reload
+			pos, circle := tw.getPositionAndCircle(task.delay)
+			task.pos = pos
+			task.circle = circle
+			// reset
+			tw.buckets[pos].push(task)
+			tw.keyPosMap.Store(task.key, pos)
+		} else {
+			tw.keyPosMap.Delete(task.key)
 		}
 	}
 }
--- timewheel_test.go
+++ timewheel_test.go
@@ -3,43 +3,93 @@ package timewheel
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"testing"
 	"time"
 )
 
-func TestTimeWheel_AddTask(t *testing.T) {
-	tw, err := New(100*time.Millisecond, 8)
+func TestTimeWheel_AddScheduleTask(t *testing.T) {
+	tw, err := New(1000*time.Millisecond, 1024)
 	if err != nil {
 		t.Fail()
 		return
 	}
 	tw.Start()
-	tw.Start()
+	begin := time.Now()
+
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 200; j++ {
+			key := "task == " + strconv.Itoa(i) + "-" + strconv.Itoa(j)
+			_ = tw.AddScheduleTask(key, 2000*time.Millisecond, func() {
+				cost := time.Now().Sub(begin).Seconds()
+				log.Print(key+" cost =======  ", fmt.Sprintf("%.1f", cost))
+			})
+		}
+
+	}
+	time.Sleep(10 * time.Second)
+
+}
+
+func TestTimeWheel_RemoveTask(t *testing.T) {
+	tw, err := New(100*time.Millisecond, 1024)
+	if err != nil {
+		t.Fail()
+		return
+	}
 	tw.Start()
 	begin := time.Now()
-	delay := 300 * time.Millisecond
-	err = tw.AddScheduleTask("key", delay, func() {
+	_ = tw.AddScheduleTask("task_1", 500*time.Millisecond, func() {
 		cost := time.Now().Sub(begin).Seconds()
-		fmt.Println("cost =======  ", fmt.Sprintf("%.1f", cost))
-
+		log.Print("task_1 cost =======  ", fmt.Sprintf("%.1f", cost))
 	})
+	time.Sleep(2 * time.Second)
+	tw.RemoveTask("task_1")
+	time.Sleep(2 * time.Second)
+	log.Print("end")
+}
+
+func TestTimeWheel_AddTask(t *testing.T) {
+	tw, err := New(100*time.Millisecond, 1024)
 	if err != nil {
-		log.Fatal(err)
+		t.Fail()
+		return
 	}
-	time.Sleep(3 * time.Second)
-	tw.RemoveTask("key")
-	time.Sleep(5 * time.Second)
+	tw.Start()
+	begin := time.Now()
+	_ = tw.AddTask("task_2", 500*time.Millisecond, func() {
+		cost := time.Now().Sub(begin).Seconds()
+		log.Print("task_1 cost =======  ", fmt.Sprintf("%.1f", cost))
+	})
+
+	time.Sleep(2 * time.Second)
+	log.Print("end")
+}
+
+func TestTimeWheel_Stop(t *testing.T) {
+	tw, err := New(100*time.Millisecond, 1024)
+	if err != nil {
+		t.Fail()
+		return
+	}
+	tw.Start()
+	begin := time.Now()
+	_ = tw.AddTask("task_3", 500*time.Millisecond, func() {
+		cost := time.Now().Sub(begin).Seconds()
+		log.Print("task_3 cost =======  ", fmt.Sprintf("%.1f", cost))
+	})
+	time.Sleep(2 * time.Second)
+
 	tw.Stop()
-	tw.Stop()
-	tw.Stop()
-	err = tw.AddTask("key1", delay, func() {
-		fmt.Println("stop and add task")
+
+	err = tw.AddTask("task_4", 500*time.Millisecond, func() {
+		cost := time.Now().Sub(begin).Seconds()
+		log.Print("task_4 cost =======  ", fmt.Sprintf("%.1f", cost))
 	})
 	if err == NotRunError {
-		fmt.Println("tw closed")
+		log.Print("already stop")
 	} else {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-
-	time.Sleep(10 * time.Second)
+	time.Sleep(2 * time.Second)
 }
